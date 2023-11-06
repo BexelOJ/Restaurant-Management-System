a98313d336542b78b9e5d7a7e3ce48ee18ae9423
BEXEL O J <40738158+BexelOJ@users.noreply.github.com>
2023-11-06 23:11:12 +0530

    Add files via upload
    Eclipse JDBC MySQL Init

---
+++ restaurant-management-system-master/lecture-04-submitting-sql-queries/jdbc-query/src/JdbcDemo.java
@@ -0,0 +1,49 @@
+import java.sql.*;
+
+public class JdbcDemo {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		String dbUrl = "jdbc:mysql://localhost:3306/demo";
+	               	  //jdbc:mysql://127.0.0.1:3306/?user=student
+		String user = "student";		
+		String pass = "student";
+
+		try {
+			// 1. Get a connection to database
+			myConn = DriverManager.getConnection(dbUrl, user, pass);
+			
+			// 2. Create a statement
+			myStmt = myConn.createStatement();
+			
+			// 3. Execute SQL query
+			myRs = myStmt.executeQuery("select * from employees");
+			
+			// 4. Process the result set
+			while (myRs.next()) {
+				System.out.println(myRs.getString("first_name") + " " + myRs.getString("last_name") + " , eMail - " + myRs.getString("email"));
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			if (myRs != null) {
+				myRs.close();
+			}
+			
+			if (myStmt != null) {
+				myStmt.close();
+			}
+			
+			if (myConn != null) {
+				myConn.close();
+			}
+		}
+	}
+
+}
